5bb1e0e19bac3c5451cd55d35b5cc2ac4c421710
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-01-03 14:04:08 +0530

    Summation Using For Loop

---
+++ addFunction.js
@@ -0,0 +1,16 @@
+// Fix the function
+
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+
+function addFive(num) {
+    // P:Take in a number
+    //   return with num and five add to it
+      return num+5;
+    }
+    // P:Add five to any number passed to it.
+    // R:return the number with 5 added
+    // E:addFive(5)->10;
+    // E:addFive(0)->5;
