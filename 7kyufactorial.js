4ec90e9d1bf95cdbe15d99c651606287c86b068d
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-02-04 23:40:46 +0530

    coding Challenge

---
+++ 7kyufactorial.js
@@ -0,0 +1,13 @@
+// Your task is to write function factorial.
+function factorial(n){
+    //your code here
+    return factorial =1;
+    if(n==0){
+      return  1
+    } else if(n>1){
+      for(i=1;i<=n;i++){
+        factorial=factorial*i
+      }
+    }
+    return factorial;
+  }
