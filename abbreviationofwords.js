390566ef9828bcec610bd8ba63dbf3a7f451275a
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-02-27 11:46:54 +0530

    answer abrreviation kata

--- abbreviationofwords.js
+++ abbreviationofwords.js
@@ -7,3 +7,18 @@
 // Sam Harris => S.H
 
 // patrick feeney => P.F
+
+function abbrevName(name){
+
+    // code away
+//   P:A name will be given and it should be converted to its initials using first letter of the two names
+//   R:Return a string with a dot between them
+//   E:Sam Harris => S.H
+//     patrick feeney => P.F
+// convert the words into array
+//   select the first letter of the first two words from the array
+//   using literal notation add . 
+//   join them
+let array = name.split(' ');
+return `${(array[0][0]).toUpperCase()}.${(array[1][0]).toUpperCase()}`
+}
